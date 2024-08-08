e4b9b8b9f677742761e0609a8fee3abc38bc65fe
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-08-08 12:08:53 +0300

    "hmvbhm"

--- ServiceAFront/Js/index.js
+++ ServiceAFront/Js/index.js
@@ -1,25 +1,46 @@
 
- 
-const  jsonFilePath =  '/Users/shalom_bergman/kodcode2/IronDomeProject/attackServer/ServerBeckend/missiles.json';
+const misseles = []
+const addMissile = () => {      
+    const missilesDiv = document.getElementById("missiles-div")
+    const p = document.createElement("p");
+    p.innerText = misseles.shift();
+    missilesDiv.appendChild(p);  
+}
+
+let missiles;
+const loadMissilesJson = async() => {
+    const response = await fetch(jsonFilePath);
+    const jsonData = await response.json();    
+    //console.log(`result is : ${JSON.stringify(jsonDataList)}`)
+    return jsonData;
+    
+}
+var jsonData = await loadMissilesJson();
+var jsonDataList = [];
+
+
+    
+
+console.log(jsonDataList);
+
+const  jsonFilePath =  '/ServiceAFront/Html/missiles.json';
 
 
 //const missels = JSON.parse(misselsJson)
- async function fetchJSONData() {
- try{
-    const response = await fetch(jsonFilePath)       
-            if (!response.ok) {
-                throw new Error (`HTTP error! Status: ${response.status}`);                   
-            }
-            const jsonData = await response.json();
-            console.log(JSON.stringify(jsonData));
+//  async function fetchJSONData() {
+//  try{
+//     const response = await fetch(jsonFilePath)       
+//             if (!response.ok) {
+//                 throw new Error (`HTTP error! Status: ${response.status}`);                   
+//             }
+//             const jsonData = await response.json();
+//             console.log(JSON.stringify(jsonData));
 
-            const jsonDataList = [jsonData];
-            return jsonDataList;
-        }catch(error) {
-            console.error("Unable to fetch data:", error);
-        }
- }
+//             const jsonDataList = [jsonData];
+//             return jsonDataList;
+//         }catch(error) {
+//             console.error("Unable to fetch data:", error);
+//         }
+//  }
         
- fatchJSONData().then(jsonDataList => {
-    console.log("Data saved in memory:", jsonDataList);
- });
+ 
