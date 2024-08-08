648b5b823dbfab3bf68db3a734169f87bd977be7
shalom bergman <shalom195295@gmail.com>
2024-08-08 17:47:18 +0300

    queue m

---
+++ ServerBeckend/MissilesQueue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attackServer.ServerBeckend
+{
+    internal class MissilesQueue
+    {
+
+    }
+}
