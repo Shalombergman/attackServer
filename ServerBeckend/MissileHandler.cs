801f7763dd34821a98069fe981fcb681f9e91299
shalom bergman <shalom195295@gmail.com>
2024-08-08 12:09:40 +0300

    ll,l

---
+++ ServerBeckend/MissileHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Text.Json;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace attackServer.ServerBeckend
+{
+    internal class MissileHandler
+    {
+        private readonly WebSocketServer _wss;
+        public MissileHandler(WebSocketServer wss)
+        {
+            _wss = wss;
+        }
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("data got is: " + e.Data);
+        }
+    }
+}
--- ServerBeckend/Program.cs
+++ ServerBeckend/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServerBeckend
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Threading;
+using attackServer.ServerBeckend;
+using WebSocketSharp.Server;
+namespace attackServer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            WebSocketServer wss = new WebSocketServer("ws://localhost:3108");
+            wss.AddWebSocketService<MissileHandler>("C:\\kodcode2\\Core8\\attackServer\\ServerBeckend\\MissileHandler.cs", () => new MissileHandler(wss));
+            wss.Start();
+            Console.WriteLine("Backend server is running. Press Enter to exit...");
+            Console.ReadLine();
+            wss.Stop();
+
+
         }
     }
 }
